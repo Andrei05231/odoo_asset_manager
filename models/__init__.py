2d216125b8ed458c6cd6868210ef6373bc2286eb
Andrei05231 <carare.andrei57@gmail.com>
2026-01-09 14:13:25 +0000

    add auto generated inventory code functionality do other devices

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from . import computer, monitor, history
 from .financial import finance_project
 from .software import license, software
 from .devices import printer, other
+from .inventory import inventory, res_company
--- models/devices/other.py
+++ models/devices/other.py
@@ -1,10 +1,27 @@
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 class Other(models.Model):
     _name = "assets_other"
 
     name = fields.Char(string="Name")
     inventory = fields.Char(string="Inventory Number")
+
+    inventory_number_id = fields.Many2one(
+        'asset.inventory.number',
+        string="Inventory",
+        readonly=True,
+        copy=False
+    )
+
+    inventory_code = fields.Char(
+        string="Inventory Code",
+        related='inventory_number_id.code',
+        readonly=True,
+        store=True
+    )
+
+
     serial = fields.Char(string="Serial Number")
     project_id = fields.Many2one('assets_project', string="Project")
     company_id = fields.Many2one('res.company',
@@ -20,3 +37,25 @@ class Other(models.Model):
         ('active', "Active")
     ],
         string="Asset Status")
+    
+    def action_generate_inventory_number(self):
+        """
+        Button action:
+        - Requires company
+        - Generates inventory number ONCE
+        """
+
+        for asset in self:
+            if asset.inventory_number_id:
+                continue
+
+            if not asset.company_id:
+                raise UserError(
+                    "Please set the project (company) before generating an inventory number."
+                )
+
+            inventory_number = self.env['asset.inventory.number'].generate_next(
+                asset.company_id
+            )
+
+            asset.inventory_number_id = inventory_number.id
