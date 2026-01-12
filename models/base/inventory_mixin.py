9f79ecdd7be5e77977fadb9ba4f4ad1acc2a145f
Andrei05231 <carare.andrei57@gmail.com>
2026-01-12 08:31:55 +0000

    add function for migrating Char inventory number to inventory_number_id

--- models/base/inventory_mixin.py
+++ models/base/inventory_mixin.py
@@ -1,6 +1,11 @@
 from odoo import models, fields
 from odoo.exceptions import UserError
 
+import re
+import logging
+
+_logger = logging.getLogger(__name__)
+
 class InventoryNumberMixin(models.AbstractModel):
     _name = "assets_inventory_mixin"
 
@@ -21,3 +26,74 @@ class InventoryNumberMixin(models.AbstractModel):
             inventory_number = self.env['asset.inventory.number'].generate_next(asset.company_id)
 
             asset.inventory_number_id = inventory_number.id
+
+
+    def migrate_inventory_numbers(self):
+        Inventory = self.env['asset.inventory.number']
+
+        assets = self.search([
+            ('inventory', '!=', False),
+            ('inventory_number_id', '=', False),
+            ('company_id', '!=', False),
+        ])
+
+        _logger.info("Starting inventory migration: %s assets found", len(assets))
+
+        for asset in assets:
+            _logger.info(
+                "Processing asset ID=%s inventory='%s' company_id=%s",
+                asset.id,
+                asset.inventory,
+                asset.company_id.id if asset.company_id else None,
+            )
+
+            match = re.search(r'(\d+)', asset.inventory or '')
+            if not match:
+                _logger.warning(
+                    "NO MATCH for asset ID=%s inventory='%s'",
+                    asset.id, asset.inventory
+                )
+                continue
+
+            number = int(match.group(1))
+            _logger.info("Extracted number=%s for asset ID=%s", number, asset.id)
+
+            inventory_number = Inventory.search([
+                ('company_id', '=', asset.company_id.id),
+                ('number', '=', number),
+            ], limit=1)
+
+            _logger.info(
+                "Existing inventory_number found=%s",
+                bool(inventory_number)
+            )
+
+            if not inventory_number:
+                inventory_number = Inventory.create({
+                    'company_id': asset.company_id.id,
+                    'number': number,
+                })
+                _logger.info(
+                    "CREATED inventory_number ID=%s number=%s",
+                    inventory_number.id,
+                    number,
+                )
+
+            _logger.info(
+                "Writing inventory_number_id=%s to asset ID=%s",
+                inventory_number.id,
+                asset.id,
+            )
+
+            asset.write({'inventory_number_id': inventory_number.id})
+
+            _logger.info(
+                "AFTER WRITE asset.inventory_number_id=%s",
+                asset.inventory_number_id.id,
+            )
+
+        self.env.cr.commit()
+        _logger.info("Inventory migration committed")
+
+
+
