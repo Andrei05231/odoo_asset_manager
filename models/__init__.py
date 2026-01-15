ea7b4d3e6b664b69986794189789c41db0767215
Andrei05231 <carare.andrei57@gmail.com>
2026-01-15 08:38:15 +0000

    add valiation errors to compute_score

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,4 @@ from .financial import finance_project
 from .software import license, software
 from .devices import printer, other, phone_tablet
 from .inventory import inventory, res_company
-from .scoring import score_mixin, gpu_score, cpu_score
+from .scoring import component_score
--- models/computer/compute_score.py
+++ models/computer/compute_score.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.exceptions import ValidationError
 
 class Computer(models.AbstractModel):
     _inherit = "assets_computer"
@@ -20,32 +21,36 @@ class Computer(models.AbstractModel):
             cpu_score = 0.0
             gpu_score = 0.0
 
-            if record.cpu:
-                cpu = score.search(
-                    [   ('component_type','=','cpu'),
-                        ('name','=',record.cpu)],
-                    limit = 1
-                )
-                cpu_score = cpu.score if cpu else 0
-            else : cpu_score = 0
+            if not record.cpu:
+                raise ValidationError("Record doesn t contain a CPU")
+            cpu = score.search(
+                [   ('component_type','=','cpu'),
+                    ('name','=',record.cpu)],
+                limit = 1
+            )
+            if not cpu: 
+                raise ValidationError(f'No score found for CPU: %s'%record.cpu)
+            cpu_score = cpu.score 
 
-            if record.gpu:
-                gpu = score.search(
-                    [   ('component_type','=','gpu'),
-                        ('name','=', record.gpu)],
-                    limit = 1
-                )
-                gpu_score = gpu.score if gpu else 0
-            else: gpu_score = 0
+            if not record.gpu:
+                raise ValidationError('Record doesn t contain a GPU')
+            gpu = score.search(
+                [   ('component_type','=','gpu'),
+                    ('name','=', record.gpu)],
+                limit = 1
+            )
+            if not gpu:
+                raise ValidationError(f'No score found for GPU: %s'%record.gpu)
+            gpu_score = gpu.score
 
-        gpu = self.normalize(gpu_score, gpu_max)
-        cpu = self.normalize(cpu_score, cpu_max)
+            gpu = self.normalize(gpu_score, gpu_max)
+            cpu = self.normalize(cpu_score, cpu_max)
 
-        main_score = (0.6*gpu)+(0.4*cpu)
+            main_score = (0.6*gpu)+(0.4*cpu)
 
-        record.cpu_score = cpu
-        record.gpu_score = gpu
-        record.performance_score = main_score
+            record.cpu_score = cpu
+            record.gpu_score = gpu
+            record.performance_score = main_score
 
 
     
