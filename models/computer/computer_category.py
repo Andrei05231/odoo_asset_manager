52cde6b6dafedb92184da6487b8b3ed26a89897d
Andrei05231 <carare.andrei57@gmail.com>
2026-01-22 09:16:57 +0000

    add computer_category_model

---
+++ models/computer/computer_category.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+class ComputerCategory(models.Model):
+    _name = "assets_computer_category"
+
+    name = fields.Char()
+    
+    cpu_weight = fields.Float()
+    gpu_weight = fields.Float()
+    ram_weight = fields.Float()
+
+    max_cpu_score = fields.Float()
+    max_gpu_score = fields.Float()
